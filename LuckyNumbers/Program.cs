0e845918465bdca18da96b9562d419a43eb2ec69
SyphonCypher <tim_horta@outlook.com>
2018-09-15 11:55:07 -0400

    Finished project

--- LuckyNumbers/Program.cs
+++ LuckyNumbers/Program.cs
@@ -8,14 +8,15 @@ namespace LuckyNumbers
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
 
+            int jackpot = 500000; //Creating an int named jackpot and assigning it a value to be midified later
             int[] luckyNums = new int[6]; //Creating empty array for randomly generated lottery numbers
             int[] userRange = new int[2]; //Creating empty array defining range of lottery numbers
             int[] userNums = new int[6]; //Creating empty array to match against luckyNums
 
-            Console.WriteLine("The current jackpot is $1,000,000"); //Displaying hard coded jackpot
+            Console.WriteLine("The current jackpot is " + "$" + jackpot); //Displaying hard coded int (jackpot) to user
             Console.WriteLine("Please type a minimum number."); 
             userRange[0] = int.Parse(Console.ReadLine());
             // ^Prompting user to set minimum number for lottoNums and assigning user input as value for index 0
@@ -54,8 +55,62 @@ namespace LuckyNumbers
             Console.WriteLine("Lucky Number: " + luckyNums[5]);
             // ^Displaying randomly generated values of luckyNums in console
 
-            
+            int matches = userNums.Intersect(luckyNums).Count(); //Comparing userNums to luckyNums and counting the amount of elements in common
 
+            if (matches == 6)
+            {
+                Console.WriteLine("You guessed 6 numbers correctly!");
+                jackpot = 500000 * 6;
+                Console.WriteLine("You have won " + "$" + jackpot + ".00!");
+            }
+            else if (matches == 5)
+            {
+                Console.WriteLine("You guessed 5 numbers correctly!");
+                jackpot = 500000 * 5;
+                Console.WriteLine("You have won " + "$" + jackpot + ".00!");
+            }
+            else if (matches == 4)
+            {
+                Console.WriteLine("You guessed 4 numbers correctly!");
+                jackpot = 500000 * 4;
+                Console.WriteLine("You have won " + "$" + jackpot + "!");
+            }
+            else if (matches == 3)
+            {
+                Console.WriteLine("You guessed 3 numbers correctly!");
+                jackpot = 500000 * 3;
+                Console.WriteLine("You have won " + "$" + jackpot + ".00!");
+            }
+            else if (matches == 2)
+            {
+                Console.WriteLine("You guessed 2 numbers correctly!");
+                jackpot = 500000 * 2;
+                Console.WriteLine("You have won " + "$" + jackpot + ".00!");
+            }
+            else if (matches == 1)
+            {
+                Console.WriteLine("You guessed 1 number correctly!");
+                jackpot = 500000 * 1;
+                Console.WriteLine("You have won " + "$" + jackpot + ".00!");
+            }
+            else
+            {
+                Console.WriteLine("You have gueessed 0 numbers correctly!");
+                jackpot = 500000 * 0;
+                Console.WriteLine("You have won " + "$" + jackpot + ".00!");
+            }
+
+            Console.WriteLine("Would you like to play again?");
+            string userInput = Console.ReadLine().ToLower();
+            if (userInput == "yes")
+            {
+                Main();
+            }
+            else
+            {
+                Console.WriteLine("Thanks for Playing!");
+                return;
+            }
         }
     }
 }
